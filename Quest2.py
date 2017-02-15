f651e8138569208511be5f1abee7db3bb333e56b
DavidKozina <david.kozina@eledus.cz>
2017-02-15 19:25:40 +0100

    repair of Quest2.py

--- Quest2.py
+++ Quest2.py
@@ -3,6 +3,7 @@ sum=0;
 while (fibo[-1]+fibo[-2]<4000000):
 	fibo+=[fibo[-1]+fibo[-2]];
 for i in fibo:
-	sum+=i;
-print("\nThe sum of fibo values bellow 4 000 000 is: "+str(sum));
+	if (i%2==0):
+		sum+=i;
+print("\nThe sum of fibo even values bellow 4 000 000 is: "+str(sum));
 input();
